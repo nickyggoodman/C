fe25f77f22cabc49732b1c1372cbecae532d071d
nickyggoodman <nickyggoodman@gmail.com>
2026-02-02 21:32:27 -0500

    add new section

---
+++ initialization/initialization.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+    int width;
+    width = 5;
+    std::cout << width;
+    width = 4;
+    std::cout << width;
+
+    int width1 {7};
+    std::cout << width1;
+
+    int a;
+    int b = 5;
+    int c (6);
+    int d {7};
+    int e {};
+        
+    return 0;
+}
